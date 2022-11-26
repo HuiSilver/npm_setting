1b19ac5f47c90f20947d6a50b84440dc39664397
정희은 <huieun_7@naver.com>
2022-11-26 22:13:25 +0900

    feat(webpack) : IIFE 방식의 예시

--- src/math.js
+++ src/math.js
@@ -1,3 +1,11 @@
-function sum(a,b){
-    return a+b;
-}
+var math = math || {};
+
+(function(){
+    function sum(a,b){
+        return a+b;
+    }
+
+    //sum함수를 math 모듈 외부에서 사용하기 위한 여지를 제공하기 위해
+    math.sum = sum;
+})();
+
