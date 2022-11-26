3073ec8f30246644ae4247a74b247ff35636b439
정희은 <huieun_7@naver.com>
2022-11-26 22:25:51 +0900

    feat(webpack) : 표준모듈 방식 예시

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
+import * as math from './math.js';
+
+
 console.log(math.sum(1,2));
--- src/math.js
+++ src/math.js
@@ -1,11 +1,3 @@
-var math = math || {};
-
-(function(){
-    function sum(a,b){
-        return a+b;
-    }
-
-    //sum함수를 math 모듈 외부에서 사용하기 위한 여지를 제공하기 위해
-    math.sum = sum;
-})();
-
+export function sum(a,b){
+    return a+b;
+}
