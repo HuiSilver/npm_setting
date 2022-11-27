498630306960a4d09447582719b984c442bfbd48
정희은 <huieun_7@naver.com>
2022-11-27 22:03:07 +0900

    feat(webpack): 웹팩 로더 연습

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,16 @@ module.exports = {
         //절대경로
         path: path.resolve('./dist'),
         filename:'[name].js'
+    },
+    module:{
+        rules:[
+            {
+                //로더가 처리해야할 파일들의 패턴(정규식)
+                test:/\.js$/,
+                use: [
+                    path.resolve('./my-webpack-loader.js')
+                ]
+            }
+        ]
     }
 }
